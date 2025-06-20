5943d5bd117f1d15b582d4465a1155d071e30761
Sarthak Sanjeev <sarthaknsanjeev@gmail.com>
2025-06-20 08:39:59 +0530

    Update models.py

--- models.py
+++ models.py
@@ -5,6 +5,8 @@ db = SQLAlchemy()
 
 class Subscription(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+
+    
     user_id = db.Column(db.Integer, nullable=False)
     plan_type = db.Column(db.String(50), nullable=False)
     status = db.Column(db.String(20), default="pending")
